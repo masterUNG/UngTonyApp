c0c9d0aeb6e171bfbc95252da1d27291e272316b
ung <test@abc.com>
2022-06-12 10:51:33 +0700

    authen UI success

--- lib/states/authen.dart
+++ lib/states/authen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:ungtonyapp/utility/my_constant.dart';
+import 'package:ungtonyapp/widgets/show_button.dart';
 import 'package:ungtonyapp/widgets/show_form.dart';
 import 'package:ungtonyapp/widgets/show_image.dart';
 import 'package:ungtonyapp/widgets/show_text.dart';
+import 'package:ungtonyapp/widgets/show_text_button.dart';
 import 'package:ungtonyapp/widgets/show_title.dart';
 
 class Authen extends StatefulWidget {
@@ -14,32 +16,94 @@ class Authen extends StatefulWidget {
 
 class _AuthenState extends State<Authen> {
   bool redEye = true;
+  bool remember = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          children: [
-            newLogo(),
-            newAppName(),
-            ShowTitle(title: 'Email'),
-            ShowForm(
-              hind: 'Email',
+        child: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints boxConstraints) {
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                newLogo(boxConstraints: boxConstraints),
+                newAppName(),
+                const ShowTitle(title: 'Email'),
+                newEmail(),
+                const ShowTitle(title: 'Password'),
+                newPassword(),
+                newRemember(context),
+                buttonSignIn(boxConstraints),
+                newForgot(),
+                newSignUp()
+              ],
             ),
-            ShowTitle(title: 'Password'),
-            ShowForm(
-              hind: 'Password',
-              obsecu: redEye,
-              redEyeFunc: () {
-                setState(() {
-                  redEye = !redEye;
-                });
-              },
-            ),
-          ],
-        ),
+          );
+        }),
       ),
+    );
+  }
+
+  Row newSignUp() {
+    return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const ShowText(label: "Don't have an account ? "),
+                  ShowTextButton(label: 'Sign Up', pressFunc: () {}),
+                ],
+              );
+  }
+
+  ShowTextButton newForgot() =>
+      ShowTextButton(label: 'Forgot the Password ?', pressFunc: () {});
+
+  Container buttonSignIn(BoxConstraints boxConstraints) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      width: boxConstraints.maxWidth,
+      child: ShowButton(
+        label: 'Sign In',
+        pressFunc: () {},
+      ),
+    );
+  }
+
+  Theme newRemember(BuildContext context) {
+    return Theme(
+        data: Theme.of(context).copyWith(
+          unselectedWidgetColor: MyConstant.primary,
+        ),
+        child: CheckboxListTile(
+            activeColor: MyConstant.primary,
+            controlAffinity: ListTileControlAffinity.leading,
+            title: ShowText(
+              label: 'Remember Me',
+              textStyle: MyConstant().h3ActiveStyle(),
+            ),
+            value: remember,
+            onChanged: (value) {
+              setState(() {
+                remember = value!;
+              });
+            }));
+  }
+
+  ShowForm newPassword() {
+    return ShowForm(
+      hind: 'Password',
+      obsecu: redEye,
+      redEyeFunc: () {
+        setState(() {
+          redEye = !redEye;
+        });
+      },
+    );
+  }
+
+  ShowForm newEmail() {
+    return ShowForm(
+      hind: 'Email',
     );
   }
 
@@ -50,10 +114,11 @@ class _AuthenState extends State<Authen> {
     );
   }
 
-  SizedBox newLogo() {
-    return SizedBox(
-      width: 150,
-      child: ShowImage(path: 'images/logo.png'),
+  Container newLogo({required BoxConstraints boxConstraints}) {
+    return Container(
+      margin: const EdgeInsets.only(top: 32, bottom: 8),
+      width: boxConstraints.maxWidth * 0.35,
+      child: const ShowImage(path: 'images/logo.png'),
     );
   }
 }
